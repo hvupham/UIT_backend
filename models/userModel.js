fd6ca62ac6ab2552623428cacc39467cc94d6f12
hvupham <21522804@gm.uit.edu.vn>
2024-07-31 16:58:24 +0700

    p1: create pj, api register, login

---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
