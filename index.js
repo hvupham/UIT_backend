b859a62c7dd20efd41629baceceb6307c5b4ad31
hvupham <21522804@gm.uit.edu.vn>
2024-08-31 00:18:46 +0700

    config web socket

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const { readdirSync } = require("fs");
 const connectDatabase = require("./utils/database");
+const ws = require("ws");
 const app = express();
-
 require("dotenv").config();
 
 connectDatabase();
@@ -15,9 +15,21 @@ app.use(cors());
 readdirSync("./routes").map((r) => {
   app.use("/api", require(`./routes/${r}`));
 });
+// Khởi tạo WebSocket server
+// const server = http.createServer(app);
+// initializeWebSocket(server);
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', socket => {
+  socket.on('message', message => console.log(message));
+});
+const server = app.listen(3000);
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, socket => {
+    wsServer.emit('connection', socket, request);
+  });
 });
